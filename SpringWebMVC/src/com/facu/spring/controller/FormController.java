9a2d3cf78123f37f15da45d9be4e03e8609694d1
Facundo, Mediotte <facumediotte@gmail.com>
2020-06-06 14:58:17 -0300

    SpringMVC
    Utilizando @RequestParam

--- SpringWebMVC/src/com/facu/spring/controller/FormController.java
+++ SpringWebMVC/src/com/facu/spring/controller/FormController.java
@@ -1,10 +1,11 @@
 package com.facu.spring.controller;
 
-import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class FormController {
@@ -20,9 +21,9 @@ public class FormController {
 	}
 	
 	@RequestMapping("/otherProcessForm")
-	public String otherProcessForm(HttpServletRequest request, Model model) {
-		
-		String nombre = request.getParameter("nombre");
+	//public String otherProcessForm(HttpServletRequest request, Model model) {
+	public String otherProcessForm(@RequestParam("nombre") String nombre, Model model) {
+	
 		nombre += " Mediotte";
 		String mensajeFinal = "El usuario es: " + nombre;
 		
